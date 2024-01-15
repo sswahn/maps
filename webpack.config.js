fc89edc88bf7c404f7ee336ddac943eb45c9d65e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2024-01-14 23:20:28 -0500

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require("webpack")
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(process.cwd(), 'build'),
+    publicPath: '/stackline/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(process.cwd(), 'public', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.mjs', '.*'],
+  },
+  devServer: {
+    historyApiFallback: true,
+    allowedHosts: 'all',
+  },
+  stats: {
+    errorDetails: true,
+  }
+}
